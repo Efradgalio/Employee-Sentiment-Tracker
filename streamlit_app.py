84d83c6b67b4cd84cf62602fac46ab99daaf35d8
Efradgalio <efradgamer@gmail.com>
2024-06-11 08:51:20 +0700

    add json user response save file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import time
 import random
+import json
 import streamlit as st
 
 from streamlit_option_menu import option_menu
@@ -21,13 +22,23 @@ if selected == 'Sparkbot':
 
     responses = [
                 "Thank you, your feedback will be processed and review by our HR team.",
-                "Your Welcome, if you want to submit the feedbacks again, please refresh the website.",
+                "Your Welcome, if you want to submit the feedbacks again, please refresh the website."
             ]
 
+    # Define the message limit
+    MESSAGE_LIMIT = 1
 
     # Initialize session state if not already done
     if 'step' not in st.session_state:
         st.session_state.step = 0
+
+    # Initialize session state for the message count
+    if 'message_count' not in st.session_state:
+        st.session_state.message_count = 0
+
+
+    def send_message():
+        st.session_state.message_count += 1
 
     def get_next_response():
         # Get the next response based on the current step
@@ -54,20 +65,37 @@ if selected == 'Sparkbot':
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # Accept user input
-    if prompt := st.chat_input("What is up?"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
 
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            response = st.write_stream(get_next_response())
+    if st.session_state.message_count < MESSAGE_LIMIT:
+        # Accept user input
+        if prompt := st.chat_input("What is up?"):
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            # Access user feedbacks
+            user_employee_feedbacks = st.session_state.messages[-1]
+            # Save the last message to a JSON file
+            file_path = 'user_employee_feedbacks/user_employee_feedbacks.json'
+            with open(file_path, 'r+') as f:
+                all_user_response = json.load(f)
+                all_user_response['user_responses'].append(user_employee_feedbacks)
+                f.seek(0)
+                json.dump(all_user_response, f, indent=4)
+
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                response = st.write_stream(get_next_response())
+
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+            send_message()
+    else:
+        st.write("You have reached the message limit. Please refresh the page to start a new conversation.")
+
 
 if selected == 'Employee Tracker':
     st.title(f'Employee Tracker')
@@ -80,7 +108,7 @@ if selected == 'Contact Us':
 
 
     contact_form = """
-    <form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+    <form action="https://formsubmit.co/efradgamer@gmail.COM" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
